9ed1fe47f6c9422ceb43b0773b359c589b4333da
jettkendrick <jettkendrick@gmail.com>
2018-12-04 20:40:12 -0600

    add before action authentication

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -1,4 +1,5 @@
 class Api::CapstonesController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
 
   def index
     @capstones = Capstone.all
--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,4 +1,5 @@
 class Api::EducationsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
 
   def create
     @education = Education.new(
